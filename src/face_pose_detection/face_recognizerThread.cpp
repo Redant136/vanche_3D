f73a98154d21df5f0137495df336a8e3bff5a0e0
Redant136 <43812447+Redant136@users.noreply.github.com>
2024-09-15 00:28:48 -0400

    windows update for threads

--- src/face_pose_detection/face_recognizerThread.cpp
+++ src/face_pose_detection/face_recognizerThread.cpp
@@ -1,7 +1,7 @@
 #include "face_recognizer.hpp"
-
 #define THREADDONE_MASK 0x8000
-
+#ifndef _WIN32
+#include <pthread.h>
 static volatile int recognizer_status;
 static pthread_t thread_id;
 
@@ -28,3 +28,34 @@ int recognizer_asyncJoin(int*status)
   }
   return 0;
 }
+#else
+#include <windows.h>
+
+static volatile int recognizer_status;
+static HANDLE thread;
+
+DWORD WINAPI threaded_update(LPVOID _a)
+{
+  recognizer_status = recognizer_update();
+  recognizer_status |= THREADDONE_MASK;
+  return 0;
+}
+void recognizer_asyncUpdate()
+{
+  recognizer_status = 0;
+  thread = CreateThread(NULL, 0, threaded_update, NULL, 0, NULL);
+  if (thread == NULL)
+    recognizer_status = THREADDONE_MASK | VANCHE_FRECOG_CAMERA_READ_ERR;
+}
+
+int recognizer_asyncJoin(int *status)
+{
+  if (recognizer_status & THREADDONE_MASK)
+  {
+    *status = recognizer_status & (~THREADDONE_MASK);
+    WaitForSingleObject(thread, INFINITE);
+    return 1;
+  }
+  return 0;
+}
+#endif
